40f27d44e6345b2cf75a5d8cd5dbbdf05dab5d51
Sadık Sünbül <109942759+artredline@users.noreply.github.com>
2023-03-21 21:08:38 +0300

    Revert "İsimleri ayarlandı"
    This reverts commit dc9e5c20d918d1f7ff802c5f3bdda704a10cb736.

---
+++ deneme/Migrations/20230223094644_mig_1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2._0.Bire_Bir.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "calisanler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_calisanler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "calisanAdresler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Adres = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_calisanAdresler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_calisanAdresler_calisanler_Id",
+                        column: x => x.Id,
+                        principalTable: "calisanler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "calisanAdresler");
+
+            migrationBuilder.DropTable(
+                name: "calisanler");
+        }
+    }
+}
